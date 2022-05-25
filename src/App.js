3d3b8392623770bd75d2a8eb0b0418f04e64f8cf
Rabiul hossain <mohammadrabiul4@gmail.com>
2022-05-25 09:54:46 -0700

    practicing react state and updating state.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Profile from './components/profile';
+import Timer from './components/timer';
 
 function App() {
   return (
     <div>
       <Profile/>
+      <Timer/>
     </div>
   );
 }
